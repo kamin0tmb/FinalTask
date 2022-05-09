2d559d1cabe25c45a6a4aa2f296b9275b99da545
kamin0 <jm187@yandex.ru>
2022-05-09 22:42:51 +0300

    Задание 4 готово.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace FinalTask
 {
+    [Serializable]
+    class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Student(string name, string group, DateTime dateofbirth)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = dateofbirth;
+        }
+        static public Student[] FileRead(string source)
+        {
+            if (File.Exists(source))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                Console.WriteLine($"Чтение файла: {source}");
+                using (var fs = new FileStream(source, FileMode.Open))
+                {
+                    Student[] Students = (Student[])formatter.Deserialize(fs);
+                    Console.WriteLine("Объект десериализован");
+                    return Students;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Файл не найден:{source}");
+                return null;
+            }
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string source = $@"C:\Users\{Environment.UserName}\Desktop\Students.dat";
+            string target = $@"C:\Users\{Environment.UserName}\Desktop\Students";
+            if (Directory.Exists(target))
+            {
+                Console.WriteLine($"Папка {target} уже существует. Работа программы приостановлена.");
+            }
+            else
+            {
+                Directory.CreateDirectory(target);
+                foreach (Student student in Student.FileRead(source))
+                {
+                    string GroupFile = target + @"\" + student.Group + ".txt";
+                    using (StreamWriter sw = File.AppendText(GroupFile))
+                    {
+                        sw.WriteLine($"{student.Name}, {student.DateOfBirth}");
+                    }
+                }
+                Console.WriteLine($"Файлы созданы в папке: {target}");
+            }
+
         }
     }
 }
+
+
